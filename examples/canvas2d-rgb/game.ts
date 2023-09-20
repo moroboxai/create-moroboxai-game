5b366eee9cf0d4128f7e57e7c46523e55402f36f
Nauja <jeremymorosi@hotmail.fr>
2023-09-20 18:29:54 +0200

    fix rgb

--- examples/canvas2d-rgb/game.ts
+++ examples/canvas2d-rgb/game.ts
@@ -34,8 +34,8 @@ class Game implements MoroboxAIGameSDK.IGame {
             let i = 0;
             for (let y = 0; y < this.height; ++y) {
                 for (let x = 0; x < this.width; ++x) {
-                    data[i] = x;
-                    data[i+1] = y;
+                    data[i] = x * 2;
+                    data[i+1] = y * 2;
                     data[i+2] = 0;
                     data[i+3] = 255;
                     i += 4;
